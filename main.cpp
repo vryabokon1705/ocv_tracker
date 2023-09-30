ce28a7fc495ab3e18c58e950b460ef9d254ee273
Vsevolod Riabokon <vsevolod.ryabokon@gmail.com>
2023-09-30 18:40:15 +0300

    Roi percentage check

--- main.cpp
+++ main.cpp
@@ -17,6 +17,12 @@ int main(int argc, char *argv[])
         return 0;
     }
 
+    int roiPercentage = stoi(argv[3]);
+    if(roiPercentage <= 0 || roiPercentage >= 100) {
+        cerr << "Roi percentage cannot be less than 0 and greater than 100" << endl;
+        return 1;
+    }
+
     shared_ptr<Player> player = make_shared<Player>(argv[1], argv[2], stoi(argv[3]), argv[4], argv[5]);
     player->readFile();
     return 0;
